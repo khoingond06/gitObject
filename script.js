828ce2a26c5b42ea9bc3a3f9737d29a0bde951b0
khoind <khoingond06@gmail.com>
2026-04-10 17:01:14 +0700

    2

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ let users = [
   { id: 2, name: "B", age: 22 }
 ];
 
-
+// thêm
 users.push({ id: 3, name: "C", age: 25 });
 console.log(users);
 
+// sửa
 let user = users.find(u => u.id === 2);
 if (user) user.name = "B";
 console.log(users);
 
 
+//xóa
 users = users.splice(1);
 console.log(users);
