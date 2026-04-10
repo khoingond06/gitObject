e601b535776a7c513afa7fb549fc2d6fbe5c1555
khoind <khoingond06@gmail.com>
2026-04-10 16:59:48 +0700

    1

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 let users = [
-  { id: 1, name: "An", age: 20 },
-  { id: 2, name: "Bình", age: 22 }
+  { id: 1, name: "A", age: 20 },
+  { id: 2, name: "B", age: 22 }
 ];
 
-// Thêm
-users.push({ id: 3, name: "Cường", age: 25 });
+
+users.push({ id: 3, name: "C", age: 25 });
 console.log(users);
-// Sửa
+
 let user = users.find(u => u.id === 2);
-if (user) user.name = "Bình Pro";
+if (user) user.name = "B";
 console.log(users);
 
-// Xóa
-users = users.splice(1);
 
+users = users.splice(1);
 console.log(users);
